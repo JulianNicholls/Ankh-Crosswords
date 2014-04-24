e7bdd792d09160f542d0496ab5369a28f8d7a29e
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-24 19:28:38 +0100

    Resource Loader - Just fonts so far

---
+++ resources.rb
@@ -0,0 +1,33 @@
+require 'gosu_enhanced'
+
+module Crossword
+  # Resource Loader
+  class ResourceLoader
+    def self.fonts( window )
+      {
+        cell:       Gosu::Font.new( window, 'Arial', 14 ),
+        number:     Gosu::Font.new( window, 'Arial', 9 ),
+        clue:       Gosu::Font.new( window, 'Arial', 14 ),
+        header:     Gosu::Font.new( window, 'Arial', 24 ) 
+      }
+    end
+
+=begin
+    def self.images( window )
+      {
+        background: Gosu::Image.new( window, 'media/background.png', true ),
+        letter:     Gosu::Image.new( window, 'media/letter-bg.png', true ),
+        selected:   Gosu::Image.new( window, 'media/letter-selected-bg.png', true )
+      }
+    end
+
+    def self.sounds( window )
+      {
+        ok:   Gosu::Sample.new( window, 'media/ok.wav' ),
+        uhuh: Gosu::Sample.new( window, 'media/uhuh.wav' ),
+        blip: Gosu::Sample.new( window, 'media/blip.wav' )
+      }
+    end
+=end
+  end
+end
