393362cddd2fbe90d986cfb3ab1419ac175936cb
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-24 16:43:28 +0100

    Displays clues as well as the other info

--- load_la.rb
+++ load_la.rb
@@ -1,22 +1,28 @@
+require 'pp'
+
 require './puzloader'
+require './crosswordgrid'
 
 def debug( name, value )
-  printf "%s: %d %04x\n", name, value, value
+  printf "%-10s: %d %04x\n", name, value, value
 end
 
 puz = PuzzleLoader.new( '2014-4-22-LosAngelesTimes.puz' )
 
-debug 'Width ', puz.width
+debug 'Width', puz.width
 debug 'Height', puz.height
-debug 'Clues ', puz.num_clues
+debug 'Clues', puz.num_clues
 debug 'Scrambled', puz.scrambled? ? 1 : 0
 
 puts %(
-Title:  #{puz.title}
-Author: #{puz.author}
-Copy:   #{puz.copyright}
+Title:      #{puz.title}
+Author:     #{puz.author}
+Copyright:  #{puz.copyright}
+
 )
 
-puz.rows.each { |row| puts row }
+puz.clues.each_slice( 2 ) { |clue| puts clue.join ' :: ' }
 
-puz.clues.each { |clue| puts clue }
+cgrid = CrosswordGrid.new( puz.rows )
+
+pp cgrid.grid
