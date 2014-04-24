bf5e6f2726171c1145aa7c7d7bc5d12afa80c387
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-24 19:28:48 +0100

    Enlarged window and added clue column size.

--- constants.rb
+++ constants.rb
@@ -7,13 +7,15 @@ module Crossword
 
     MARGIN      = 5
 
-    BASE_WIDTH  = MARGIN * 2
-    BASE_HEIGHT = MARGIN * 2
+    BASE_WIDTH  = MARGIN * 4 + 600
+    BASE_HEIGHT = MARGIN * 4
 
-    GRID_ORIGIN = Point.new( MARGIN, MARGIN )
+    GRID_ORIGIN = Point.new( MARGIN * 2, MARGIN * 2 )
 
     CELL_SIZE   = Size.new( 28, 28 )
 
+    CLUE_COLUMN_WIDTH = 290
+     
     WHITE       = Gosu::Color.new( 0xffffffff )
     BLACK       = Gosu::Color.new( 0xff000000 )
   end
