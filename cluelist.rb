fc303dac584e674ec4b06d559ad96ef3305368d7
Julian Nicholls <juliannicholls29@gmail.com>
2014-05-23 16:28:23 +0100

    Cluelist is now Grid::Cluelist

--- cluelist.rb
+++ cluelist.rb
@@ -1,57 +1,59 @@
 require 'clue'
 
 module Crossword
-  # Hold the lists of clues
-  class ClueList
-    def initialize
-      @clues = []
-    end
+  class Grid
+    # Hold the lists of clues
+    class ClueList
+      def initialize
+        @clues = []
+      end
 
-    def add( clue )
-      @clues << clue
-    end
+      def add( clue )
+        @clues << clue
+      end
 
-    def across_clues
-      @clues.select { |c| c.direction == :across }
-    end
+      def across_clues
+        @clues.select { |c| c.direction == :across }
+      end
 
-    def down_clues
-      @clues.select { |c| c.direction == :down }
-    end
+      def down_clues
+        @clues.select { |c| c.direction == :down }
+      end
 
-    def clues( direction )
-      direction == :across ? across_clues : down_clues
-    end
+      def clues( direction )
+        direction == :across ? across_clues : down_clues
+      end
 
-    def first_clue( direction )
-      clues( direction ).first.number
-    end
+      def first_clue( direction )
+        clues( direction ).first.number
+      end
 
-    def next_clue( start, direction )
-      list = clues( direction )
+      def next_clue( start, direction )
+        list = clues( direction )
 
-      idx = list.index { |clue| clue.number >= start }
+        idx = list.index { |clue| clue.number >= start }
 
-      fail "next: idx == nil, start: #{start}, dir: #{direction}" if idx.nil?
+        fail "next: idx == nil, start: #{start}, dir: #{direction}" if idx.nil?
 
-      list[[idx + 1, list.size - 1].min].number
-    end
+        list[[idx + 1, list.size - 1].min].number
+      end
 
-    def prev_clue( start, direction )
-      list = clues( direction )
+      def prev_clue( start, direction )
+        list = clues( direction )
 
-      idx = list.rindex { |clue| clue.number <= start }
+        idx = list.rindex { |clue| clue.number <= start }
 
-      fail "prev: idx == nil, start: #{start}, dir: #{direction}" if idx.nil?
+        fail "prev: idx == nil, start: #{start}, dir: #{direction}" if idx.nil?
 
-      list[[idx - 1, 0].max].number
-    end
+        list[[idx - 1, 0].max].number
+      end
 
-    def cell_number( num, direction )
-      clue  = clues( direction ).find { |c| c.number == num }
-      return clue.point unless clue.nil?
+      def cell_number( num, direction )
+        clue  = clues( direction ).find { |c| c.number == num }
+        return clue.point unless clue.nil?
 
-      fail "Didn't find #{num} #{direction}"
+        fail "Didn't find #{num} #{direction}"
+      end
     end
   end
 end
