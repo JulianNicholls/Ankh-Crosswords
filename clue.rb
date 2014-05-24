237becfe98118cb8d1ce75aee2e5e1ea0b281801
Julian Nicholls <juliannicholls29@gmail.com>
2014-05-24 21:11:59 +0100

    draw returns height.
    Refactored per rubocop.

--- clue.rb
+++ clue.rb
@@ -14,24 +14,18 @@ module Crossword
     end
 
     def draw( game, pos, max_width, selected )
-      font = game.font[:clue]
-
-      size  = font.measure( text )
+      size  = game.font[:clue].measure( text )
       tlc   = pos.dup
 
-      font.draw( number, pos.x, pos.y, 2, 1, 1, WHITE )
+      game.font[:clue].draw( number, pos.x, pos.y, 2, 1, 1, WHITE )
 
-      if size.width > max_width
-        draw_wrapped( game, pos, text, (size.width / max_width).ceil )
-      else
-        draw_simple( game, pos, text )
-      end
+      draw_wrapped( game, pos, text, (size.width / max_width).ceil )
 
-      pos.move_by!( 0, 1 )
-      
       @region = Region.new( tlc, Size.new( max_width, pos.y - tlc.y ) )
-      
+
       @region.draw( game, 1, CLUE_LIGHT ) if selected
+
+      size.height + 1
     end
 
     private
@@ -46,14 +40,14 @@ module Crossword
       font = game.font[:clue]
 
       font.draw( text, pos.x + 18, pos.y, 2, 1, 1, WHITE )
-      pos.move_by!( 0, font.height )
+      pos.move_by!( 0, font.height + 1 )
     end
 
     def wrap( text, pieces = 2 )
       return [text] if pieces == 1
 
       pos    = text.size / pieces
-      
+
       # Find the next and previous spaces, and ...
       nspace = text.index( ' ', pos )
       pspace = text.rindex( ' ', pos )
