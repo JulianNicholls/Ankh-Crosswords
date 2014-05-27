0b839b521bb2a854e13276e6168c2c712ef39eb0
Julian Nicholls <juliannicholls29@gmail.com>
2014-05-27 18:16:49 +0100

    Swapped grid and clue list positions.

--- constants.rb
+++ constants.rb
@@ -7,14 +7,17 @@ module Crossword
 
     MARGIN      = 5
 
-    GRID_ORIGIN = Point.new( MARGIN * 2, MARGIN * 2 )
-
     CELL_SIZE   = Size.new( 28, 28 )
 
     CLUE_COLUMN_WIDTH = 290
 
     BASE_WIDTH  = MARGIN * 8 + 2 * CLUE_COLUMN_WIDTH
     BASE_HEIGHT = MARGIN * 4
+
+    GRID_ORIGIN = Point.new( BASE_WIDTH - 2 * MARGIN, MARGIN * 2 )
+
+    ACROSS_LEFT = MARGIN * 2
+    DOWN_LEFT   = ACROSS_LEFT + MARGIN * 2 + CLUE_COLUMN_WIDTH
 
     WHITE       = Gosu::Color.new( 0xffffffff )
     BLACK       = Gosu::Color.new( 0xff000000 )
@@ -23,10 +26,10 @@ module Crossword
     CLUE_LIGHT  = Gosu::Color.new( 0xff009090 )   # Dark Cyan
     ERROR_BK    = Gosu::Color.new( 0xffffe0e0 )   # Liquid Pink
     ERROR_FG    = Gosu::Color.new( 0xffff0000 )   # Bold Red
-    
-    BK_COLOURS  = { none:     WHITE, 
-                    word:     HIGHLIGHT, 
-                    current:  CURRENT, 
+
+    BK_COLOURS  = { none:     WHITE,
+                    word:     HIGHLIGHT,
+                    current:  CURRENT,
                     wrong:    ERROR_BK }
   end
 end
