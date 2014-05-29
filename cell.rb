aea2fda8f8f04c7c3bd011a58517ba336813953b
Julian Nicholls <juliannicholls29@gmail.com>
2014-05-29 17:33:11 +0100

    Split cell out and added save/load code

---
+++ cell.rb
@@ -0,0 +1,42 @@
+module Crossword
+  # Represent a whole crossword grid
+  class Grid
+    # Represent one cell in the crossword with its solution letter, user entry,
+    # possible number, and highlight state.
+    class Cell
+      attr_reader :letter, :user, :error
+      attr_accessor :number, :highlight
+
+      def self.from_text( line )
+        l, u, n = line.split ','
+        
+        me = new( l )
+        me.number = n
+        me.user = n
+        
+        me
+      end
+      
+      def initialize( letter )
+        @letter = letter
+        @user   = ''
+        @number = 0
+        @highlight = :none
+        @error  = false
+      end
+
+      def blank?
+        @letter == '.'
+      end
+
+      def user=( ltr )
+        @user  = ltr
+        @error = user != '' && letter != user
+      end
+      
+      def to_text
+        "#{letter},#{user},#{number}"
+      end
+    end
+  end
+end
