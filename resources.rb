16188839c82c686a27f918d445279603eb40d22a
Julian Nicholls <juliannicholls29@gmail.com>
2014-05-30 19:56:07 +0100

    Added overlay titling font

--- resources.rb
+++ resources.rb
@@ -9,29 +9,8 @@ module Crossword
         number:   Gosu::Font.new( window, Gosu.default_font_name, 8 ),
         clue:     Gosu::Font.new( window, Gosu.default_font_name, 11 ),
         header:   Gosu::Font.new( window, Gosu.default_font_name, 19 ),
-        button:   Gosu::Font.new( window, Gosu.default_font_name, 11 )
-#        cell:     Gosu::Font.new( window, 'Verdana', 15 ),
-#        number:   Gosu::Font.new( window, 'Verdana', 8 ),
-#        clue:     Gosu::Font.new( window, 'Verdana', 11 ),
-#        header:   Gosu::Font.new( window, 'Verdana', 20 ),
-#        button:   Gosu::Font.new( window, 'Verdana', 11 )
+        o_title:  Gosu::Font.new( window, Gosu.default_font_name, 24 )
       }
     end
-
-#    def self.images( window )
-#      {
-#        background: Gosu::Image.new( window, 'media/background.png', true ),
-#        letter:     Gosu::Image.new( window, 'media/letter-bg.png', true ),
-#        selected:   Gosu::Image.new( window, 'media/letter-selected-bg.png', true )
-#      }
-#    end
-#
-#    def self.sounds( window )
-#      {
-#        ok:   Gosu::Sample.new( window, 'media/ok.wav' ),
-#        uhuh: Gosu::Sample.new( window, 'media/uhuh.wav' ),
-#        blip: Gosu::Sample.new( window, 'media/blip.wav' )
-#      }
-#    end
   end
 end
