f5fed1bb5a51ce95d5be74caf388634ef00aecfa
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-16 21:32:22 +0000

    Increase clue font

--- resources.rb
+++ resources.rb
@@ -9,7 +9,7 @@ module Crossword
       {
         cell:     Gosu::Font.new(16, name: default),
         number:   Gosu::Font.new(8, name: default),
-        clue:     Gosu::Font.new(11, name: default),
+        clue:     Gosu::Font.new(13, name: default),
         header:   Gosu::Font.new(19, name: default),
         o_title:  Gosu::Font.new(24, name: default)
       }
